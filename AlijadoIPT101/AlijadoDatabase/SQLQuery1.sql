42731b7ec582c41df18232b2d5aebd3658de9087
GeneveAlijado <143666434+GeneveAlijado@users.noreply.github.com>
2025-10-25 14:30:03 +0800

    Add files via upload

---
+++ AlijadoIPT101/AlijadoDatabase/SQLQuery1.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO MotoParts(PartID, Part_Name, Part_Number, Descriptions, Price)
+VALUES (NEWID(), N'Piston', N'0597', N'honda125', N'599')
---
+++ AlijadoIPT101/AlijadoDatabase/Stored Procedure/DeleteParts.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[DeleteParts]
+	@Part_ID int = NULL
+AS
+BEGIN
+	DELETE FROM [dbo].[MotorParts] WHERE Part_ID = @Part_ID;
+END
